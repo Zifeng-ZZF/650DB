59591c359afbd6f61ec10d96ab968e350c3bcc3c
zz204 <zz204@duke.edu>
2021-04-08 02:27:46 -0400

    initial tables

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@
 using namespace std;
 using namespace pqxx;
 
-int main (int argc, char *argv[]) 
-{
+// prototypes
+void createRelations(pqxx::connection & conn);
+void fillTables(pqxx::connection & conn);
+
+int main (int argc, char *argv[]) {
 
   //Allocate & initialize a Postgres connection object
   connection *C;
@@ -41,4 +44,55 @@ int main (int argc, char *argv[])
   return 0;
 }
 
+/**
+ *
+ * @param conn connection with db
+ */
+void createRelations(pqxx::connection & conn) {
+    pqxx::work txn(conn);
+    stringstream ss;
+    ss << "create table STATE ("
+       << "STATE_ID integer primary key,"
+       << "NAME varchar(30) not null )";
+    txn.exec(ss.str());
 
+    stringstream ssc;
+    ssc << "create table COLOR ("
+        << "COLOR_ID integer primary key,"
+        << "NAME varchar(30) not null )";
+    txn.exec(ssc.str());
+
+    stringstream sst;
+    sst << "create table TEAM ("
+        << "TEAM_ID integer primary key,"
+        << "NAME varchar(30) not null,"
+        << "STATE_ID integer references STATE not null,"
+        << "COLOR_ID integer references COLOR not null,"
+        << "WINS integer not null,"
+        << "LOSES integer not null )";
+    txn.exec(sst.str());
+
+    stringstream ssp;
+    ssp << "create table PLAYER ("
+        << "PLAYER_ID integer primary key,"
+        << "TEAM_ID integer references TEAM not null,"
+        << "UNIFORM_NUM integer not null,"
+        << "FIRST_NAME varchar(40) not null,"
+        << "LAST_NAME varchar(40) not null,"
+        << "MPG integer not null,"
+        << "PPG integer not null,"
+        << "RPG integer not null,"
+        << "APG integer not null,"
+        << "SPG integer not null,"
+        << "BPG integer not null )";
+    txn.exec(ssp.str());
+    txn.commit();
+}
+
+/**
+ *
+ * @param conn connection with db
+ */
+void fillTables(pqxx::connection & conn) {
+
+}
